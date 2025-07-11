467f177eecd9f2195649e6d1c68a3e2691b646c5
Kobi <constantinbiryulin@gmail.com>
2025-07-11 08:16:16 +0300

    add new branch with LessonTwo

--- src/main/java/org/example/LessonTwo.java
+++ src/main/java/org/example/LessonTwo.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class LessonTwo {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 }
