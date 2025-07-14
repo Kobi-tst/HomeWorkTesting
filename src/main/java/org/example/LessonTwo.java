b7c9f1dc8ba87108285b4010e8e1360efb4b33d6
Kobi <constantinbiryulin@gmail.com>
2025-07-14 10:24:27 +0300

    add new branch with LessonTwo

--- src/main/java/org/example/LessonTwo.java
+++ src/main/java/org/example/LessonTwo.java
@@ -9,16 +9,16 @@ public class LessonTwo {
         checkSumSign();
         printColor();
         compareNumbers();
-        fiveMetod();
-        sixthMethod();
-        seventhMethod();
-        eightMethod();
+       /fiveMetod(10, 20);
+        sixthMethod(5);
+        seventhMethod(-5);
+        eightMethod("Строка", 5);
         ninthMethod();
         tenthMethod();
         eleventhMethod();
         twelvethMethod();
         thirteenthMethod();
-        fourteenthMethod();
+        fourteenthMethod(6, 7);
     }
 
     public static void printThreeWords(){
@@ -30,23 +30,20 @@ public class LessonTwo {
     public static void checkSumSign(){
         int a = 6;
         int b = -9;
-        int c = a + b;
-        if (c >= 0) {
-            System.out.println("Число положительное");
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
         } else {
-            System.out.println("Число отрицательное");
+            System.out.println("Сумма отрицательная");
         }
     }
 
     public static void printColor(){
-        int value = 100;
+        int value = 101;
         if (value <= 0){
             System.out.println("Красный");
-        }
-        if (value > 0 && value <= 100 ){
+        } else if (value > 0 && value <= 100) {
             System.out.println("Жёлтый");
-        }
-        if (value > 100 ){
+        } else {
             System.out.println("Зелёный");
         }
     }
@@ -55,34 +52,18 @@ public class LessonTwo {
         int a = 10;
         int b = 1;
         if (a >= b){
-            System.out.println("a>=b");
+            System.out.println("a >= b");
         }
         else {
-            System.out.println("a<b");
+            System.out.println("a < b");
         }
     }
 
-    public static boolean fiveMetod(){
-        System.out.print("Введите число а: ");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.print("Введите число b: ");
-        int b = scanner.nextInt();
-        int sum = a + b;
-        if (sum >= 10 && sum <= 20) {
-            System.out.println("true");
-            return true;
-        }
-        else{
-            System.out.println("false");
-            return false;
-        }
+    public static boolean fiveMetod(int a, int b){
+        return ((a + b) >= 10 && (a + b) <= 20);
     }
 
-    public static void sixthMethod(){
-        System.out.print("Введите целое число: ");
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+    public static void sixthMethod(int a){
         if (a >= 0){
             System.out.println("Число положительное");
         }
@@ -91,26 +72,16 @@ public class LessonTwo {
         }
     }
 
-    public static boolean seventhMethod(){
-        System.out.print("Введите целое число: ");
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+    public static boolean seventhMethod(int a){
         if (a >= 0){
-            System.out.println("false");
             return false;
         }
         else {
-            System.out.println("true");
             return true;
         }
     }
 
-    public static void eightMethod(){
-        System.out.print("Введите текст: ");
-        Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        System.out.print("Введите число: ");
-        int a = scan.nextInt();
+    public static void eightMethod(String str, int a){
         for (int i = 0; i < a; i++){
             System.out.println(str);
         }
@@ -121,11 +92,9 @@ public class LessonTwo {
         Scanner scan = new Scanner(System.in);
         int year = scan.nextInt();
         if (year %4 == 0 || year %400 == 0){
-            System.out.println("Год високосный");
             return true;
         }
         else {
-            System.out.println("Год не високосный");
             return false;
         }
     }
@@ -162,24 +131,12 @@ public class LessonTwo {
     }
 
     public static void twelvethMethod(){
-        int num[] = new int [12];
-        num[0] = 1;
-        num[1] = 5;
-        num[2] = 3;
-        num[3] = 2;
-        num[4] = 11;
-        num[5] = 4;
-        num[6] = 5;
-        num[7] = 2;
-        num[8] = 4;
-        num[9] = 8;
-        num[10] = 9;
-        num[11] = 1;
-        for(int i = 0; i < 12; i++){
+        int num[] = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for(int i = 0; i < num.length; i++){
             if(num[i] < 6){
                 num[i] = num[i]*2;
             }
-            System.out.println(num[i]);
+            System.out.print(num[i] + " ");
         }
     }
 
@@ -206,19 +163,12 @@ public class LessonTwo {
         }
     }
 
-    public static void fourteenthMethod(){
-        System.out.print("Введите len: ");
-        Scanner scan = new Scanner(System.in);
-
-        int len = scan.nextInt();
+    public static int[] fourteenthMethod(int len, int initialValue){
         int[] num = new int[len];
-
-        System.out.print("Введите initialValue: ");
-        int initialValue = scan.nextInt();
-
         for (int i = 0; i < len; i++){
             num[i] = initialValue;
-            System.out.print(num[i] + " ");
+            //System.out.print(num[i] + " "); //Писал для себя, чтобы проверить
         }
+        return num;
     }
 }
