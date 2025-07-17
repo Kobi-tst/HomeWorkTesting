fe52fa7f32947640f8e5e9d6b0b5519789635ed7
Kobi <constantinbiryulin@gmail.com>
2025-07-17 09:59:54 +0300

    add new branch with LessonThree

--- src/main/java/org/example/Tovar.java
+++ src/main/java/org/example/Tovar.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Tovar {
+    private String name; //Название
+    private String datePr; //Дата производства
+    private String producer; //Производитель
+    private String country; //Страна происхождения
+    private int price; //Цена
+    private boolean status; //Состояние бронирования покупателем
+
+    Tovar(String name, String datePr, String producer, String country, int price, boolean status) {
+        this.name = name;
+        this.datePr = datePr;
+        this.producer = producer;
+        this.country = country;
+        this.price = price;
+        this.status = status;
+    }
+
+    void printTovar(){
+        System.out.println(name + " " + datePr + " " + producer + " " + country + " " + price + " " + status);
+    }
 }
