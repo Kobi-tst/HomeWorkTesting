b6ee3e4408884fba0aeb2a196007c20d7abdf23f
Kobi <constantinbiryulin@gmail.com>
2025-07-18 09:46:53 +0300

    add new branch with lessonFour

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,4 +1,22 @@
 package org.example;
 
-public class Animal {
+public abstract class Animal {
+    String name;    //Название животного
+    public static int animalQty; //Счетчик животных
+
+    public Animal(String name) {
+        this.name = name;
+        animalQty += 1;
+    }
+
+    public static void printQuantity() {
+        System.out.println("Животных: " + animalQty);
+    }
+
+    public void run(int distance) {
+        System.out.println(name + " пробежал " + distance + "м");
+    }
+    public void swim(int distance) {
+        System.out.println(name + " проплыл " + distance + "м");
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,29 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Dog dogBobik = new Dog("Бобик");
+        dogBobik.run(150);
+        dogBobik.swim(20);
+
+        Cat catBarsik = new Cat("Барсик");
+        catBarsik.run(350);
+        catBarsik.swim(20);
+
+        Cat catPushok = new Cat("Пушок");
+
+        Animal.printQuantity();
+        Dog.printQuantity();
+        Cat.printQuantity();
+
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Мурзик");
+        cats[1] = new Cat("Черныш", 15);
+        cats[2] = new Cat("Жорик", 50);
+
+        Bowl catsBowl = new Bowl(70);
+        for (Cat cat: cats) {
+            cat.eat(catsBowl);
+            cat.printWellfed();
         }
     }
 }
