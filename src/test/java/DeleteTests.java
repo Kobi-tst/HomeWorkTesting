a40924ce0444bc6fa84e3d380829c2502d11aab4
Kobi <constantinbiryulin@gmail.com>
2025-08-06 11:58:18 +0300

    add new branch with lessonEight

---
+++ src/test/java/DeleteTests.java
@@ -0,0 +1,9 @@
+import org.example.*;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeClass;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class DeleteTests {
+}
--- src/test/java/GetTests.java
+++ src/test/java/GetTests.java
@@ -1,2 +1,33 @@
+import org.example.*;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.BeforeClass;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class GetTests {
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "https://postman-echo.com";
+    }
+    @Test
+    public void getTests() {
+        given()
+                .queryParam("foo1", "bar1")
+                .queryParam("foo2", "bar2")
+                .when()
+                .get("/get")
+                .then()
+                .statusCode(200)
+                .body("args.foo1", equalTo("bar1"))
+                .body("args.foo2", equalTo("bar2"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-request-start", startsWith("t="))
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.accept", equalTo("*/*"))
+                .body("headers['user-agent']", notNullValue())
+                .body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
+    }
 }
